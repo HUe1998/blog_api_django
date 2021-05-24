13b41f07c28436b20e34f8ab755829fa155d2b5b
HUe1998 <wasddsawwasddsawwasd@gmail.com>
2021-05-24 13:18:45 +0530

    Add couple of api's to our blog app
    A 'PostList' api to list all post and create new ones. A 'PostDetail'
    api to retrieve details about a post, update them or delete that post.

--- blog_api_django/settings.py
+++ blog_api_django/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # local
     'posts',
+    # 3rd party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+'DEFAULT_PERMISSION_CLASSES': [
+'rest_framework.permissions.AllowAny',
+]
+}
